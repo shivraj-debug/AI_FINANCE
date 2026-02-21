2c3a88b682ada8c3f7278b60a19077418105415b
Shiv Raj <shiv@Shivs-MacBook-Air.local>
2026-02-22 01:41:08 +0530

    changes in middleware.js

--- middleware.js
+++ middleware.js
@@ -1,36 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-// 🔥 ArcJet setup
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        "GO_HTTP",
-      ],
-    }),
-  ],
-});
-
-// 🔥 NextAuth protection
-const authMiddleware = withAuth(
-  function middleware(req) {
-    return NextResponse.next();
-  },
-  {
-    pages: {
-      signIn: "/login", // redirect to your custom login page
-    },
-  }
-);
-
-// 🔥 Chain: ArcJet first, then NextAuth
-export default createMiddleware(aj, authMiddleware);
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
